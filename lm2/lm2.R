a3108b05300b5f963ee49e43ccac0e359056854d
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-05-24 11:42:13 +0700

    Use lm2 (BiDimRegression) With (In) R Software

--- lm2/lm2.R
+++ lm2/lm2.R
@@ -6,7 +6,7 @@ install.packages("BiDimRegression")
 library("BiDimRegression")
 lm2 = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lm2/main/lm2/lm2.csv",sep = ";")
 # Estimation Fitting Bidimensional Regression Models Use lm2 (BiDimRegression) With (In) R Software
-flm2_euclidean <- lm2(depV1 + depV2 ~ indepV1 + indepV2, lm2, 'euclidean')
+lm2_euclidean <- lm2(depV1 + depV2 ~ indepV1 + indepV2, lm2, 'euclidean')
 lm2_affine <- lm2(depV1 + depV2 ~ indepV1 + indepV2, lm2, 'affine')
 lm2_projective <- lm2(depV1 + depV2 ~ indepV1 + indepV2, lm2, 'projective')
 summary(lm2_euclidean)
